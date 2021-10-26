6986dea9d32cdf3ece3c7e2f6e31d58a0c0b578a
Mozdir Taras <letsokey@mail.ru>
2021-10-26 14:05:06 +0400

    Реализация Отчет для налоговой

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,26 @@
 package org.example;
 
+import java.util.concurrent.atomic.AtomicIntegerArray;
+
 /**
  * Main Class!
- *
  */
-public class Main
-{
-    public static void main( String[] args )
-    {
-        
+public class Main {
+    public static void main(String[] args) {
+        Shop shop = new Shop();
+        // Созданём 3 потока магазинов с разной выручкой
+        Thread shop1 = new Thread(() -> shop.shopCash(new int[]{300, 500, 600, 1000}), "Магазин 1");
+        Thread shop2 = new Thread(() -> shop.shopCash(new int[]{400, 100}), "Магазин 2");
+        Thread shop3 = new Thread(() -> shop.shopCash(new int[]{200, 100, 500, 600, 2000, 4000}), "Магазин 3");
+        shop1.start();
+        shop2.start();
+        shop3.start();
+        while (true) {
+            //Как только все 3 магазина завершают подсчёт - выводим общую сумму по всем магазинам.
+            if (shop1.isInterrupted() && shop2.isInterrupted() && shop3.isInterrupted()) {
+                shop.showAllCash();
+                break;
+            }
+        }
     }
 }
---
+++ src/main/java/org/example/Shop.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Класс магазинов
+ */
+public class Shop {
+
+    //Атомик, который ведёт общий подсчёт для всех магазинов
+    AtomicLong atomicSum = new AtomicLong(0);
+
+    /**
+     * Метод для подсчёта выручки магазина, принимает массив с транзакциями
+     * (целые положительные числа)
+     * Также добавляет сумму выручки в общую кассу atomicSum
+     */
+    public void shopCash(int[] mass) {
+        int sumShopCash = 0;
+        for (int i : mass) {
+            atomicSum.addAndGet(i);
+            sumShopCash = sumShopCash + i;
+        }
+        Thread thread = Thread.currentThread();
+        System.out.println("Выручка для: " +
+                thread.getName() + " = " + sumShopCash);
+        thread.interrupt();
+    }
+
+    /**
+     * Метод выводит общую выручку по всем магазинам за день
+     */
+    public void showAllCash() {
+        System.out.println("Общая выручка магазинов: " + atomicSum);
+    }
+}
